3851d105caf8d15f072899ff2f87fad8f87a3fd3
Kaoru NAKAMURA <kaorun55@gmail.com>
2013-12-09 20:19:24 +0900

    BroadcastParserを追加

---
+++ ScratchNet/ScratchNet.Test/BroadcastParserTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScratchNet.Test
+{
+    [TestClass]
+    public class BroadcastParserTest
+    {
+        [TestMethod]
+        public void Parse()
+        {
+            var str = BroadcastParser.Parse( "broadcast \"bar\"" );
+            Assert.AreEqual( "bar", str );
+        }
+
+        [TestMethod]
+        public void IsValid()
+        {
+            Assert.IsTrue( BroadcastParser.IsValid( "broadcast \"bar\"" ) );
+        }
+
+        [TestMethod]
+        public void broadcastしかなくてIsValidがfalse()
+        {
+            Assert.IsTrue( BroadcastParser.IsValid( "broadcast " ) );
+        }
+
+        [TestMethod]
+        public void sensor_updateはIsValidがfalse()
+        {
+            Assert.IsFalse( BroadcastParser.IsValid( "sensor-update \"piyo\" 76 " ) );
+        }
+    }
+}
---
+++ ScratchNet/ScratchNet/BroadcastParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchNet
+{
+    public class BroadcastParser
+    {
+        private static string[] InnerParse( string message )
+        {
+            var content = message.Split( " ".ToCharArray() );
+            if ( content.Length != 2 ) {
+                return null;
+            }
+
+            if ( content[0] != "broadcast" ) {
+                return null;
+            }
+
+            return content;
+        }
+        
+        public static string Parse( string message )
+        {
+            var content = InnerParse( message );
+            if ( content == null ) {
+                return string.Empty;
+            }
+
+            return content[1].Replace( "\"", "" );
+        }
+
+        public static bool IsValid( string message )
+        {
+            return InnerParse( message ) != null;
+        }
+    }
+}
